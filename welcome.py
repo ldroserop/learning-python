3d23447c61e6ed392858c299aa14b697931362d3
Luis R <Ldroserop01@gmail.com>
2024-01-20 09:13:33 -0500

    Refactoring code

--- welcome.py
+++ welcome.py
@@ -1,5 +1,8 @@
 name = "Luis"
 pais = "Colombia"
+birthday = 1999
+now = 2024
+
 print("Tu nombre es: ", name, "y eres de : ", pais)
-print("Tu nombre es: ", name)
-print("y eres de : ", pais)
+
+print("Tu edad es: ", now - birthday)
